f72e2357017bc058c685af2f40e489c62a2e0d30
aitgal <aitgal@ubuntu>
2022-05-05 18:46:13 +0200

    Innit Proyecto Singleton

---
+++ PruebaSingleton/src/pruebasingleton/Singleton.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebasingleton;
+
+/**
+ *
+ * @author aitgal
+ */
+public class Singleton {
+    private static Sigleton instance = null;
+    private Singleton() {
+        //Este método existe solamente para evitar la instanciación.
+    }
+    
+    //Se utiliza el método getInstance para controlar la instanciación.
+    //solamente existira un objeto único para la clase.
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+    
+    //Se definen los distintos métodos de la clase singleton
+    protected static void dimeAlgo() {
+        System.out.println("Método de la clase singleton");
+    }
+    
+}
